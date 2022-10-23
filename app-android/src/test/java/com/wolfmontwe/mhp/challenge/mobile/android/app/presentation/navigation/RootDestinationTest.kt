458223a0b1d5ff5f8b32b7769e036c80412e235a
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-10-23 09:41:21 +0200

    Add Kotlin reflect to jUnit test dependencies to safeguard against sealed member changes

--- app-android/src/test/java/com/wolfmontwe/mhp/challenge/mobile/android/app/presentation/navigation/RootDestinationTest.kt
+++ app-android/src/test/java/com/wolfmontwe/mhp/challenge/mobile/android/app/presentation/navigation/RootDestinationTest.kt
@@ -7,16 +7,34 @@ package com.wolfmontwe.mhp.challenge.mobile.android.app.presentation.navigation
 import com.wolfmontwe.mhp.challenge.mobile.android.app.test.isOfType
 import com.wolfmontwe.mhp.challenge.mobile.android.app.test.mustEqual
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 internal class RootDestinationTest {
 
     @Test
     fun `SHOULD implement contract`() {
+        validateSealedMemberSize()
+
         RootDestination.House isOfType NavigationContract.Destination::class
     }
 
     @Test
-    fun `SHOULD have house as route`() {
+    fun `SHOULD return correct route`() {
+        validateSealedMemberSize()
+
         RootDestination.House.route mustEqual "house"
+    }
+
+    private fun validateSealedMemberSize() {
+        val subclasses = RootDestination::class.sealedSubclasses
+        assertEquals(
+            expected = ROOT_DESTINATION_MEMBER_SIZE,
+            actual = subclasses.size,
+            message = "Sealed member size changed and test needs to be adjusted accordingly"
+        )
+    }
+
+    companion object {
+        const val ROOT_DESTINATION_MEMBER_SIZE = 1
     }
 }
