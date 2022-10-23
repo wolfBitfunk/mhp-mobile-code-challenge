f7d655406e8e57b831fd339caa77acbb60aef189
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-10-23 17:52:50 +0200

    Add IdentifierMapper

---
+++ app-android/src/test/java/com/wolfmontwe/mhp/challenge/mobile/android/app/data/mapper/IdentifierMapperTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022. Wolf-Martell Montwé
+ */
+
+package com.wolfmontwe.mhp.challenge.mobile.android.app.data.mapper
+
+import com.wolfmontwe.mhp.challenge.mobile.android.app.data.DataContract.Mapper
+import com.wolfmontwe.mhp.challenge.mobile.android.app.domain.Result.Failure
+import com.wolfmontwe.mhp.challenge.mobile.android.app.domain.Result.Success
+import com.wolfmontwe.mhp.challenge.mobile.android.app.domain.entity.Identifier
+import com.wolfmontwe.mhp.challenge.mobile.android.app.test.isOfType
+import com.wolfmontwe.mhp.challenge.mobile.android.app.test.mustEqual
+
+import kotlin.test.Test
+
+class IdentifierMapperTest {
+
+    private val testSubject = IdentifierMapper()
+
+    @Test
+    fun `SHOULD implement contract`() {
+        testSubject isOfType Mapper.Id::class
+    }
+
+    @Test
+    fun `SHOULD return failure when url malformed`() {
+        testSubject.mapToDomain("example") isOfType Failure::class
+        testSubject.mapToDomain("example/124") isOfType Failure::class
+        testSubject.mapToDomain("example.com") isOfType Failure::class
+        testSubject.mapToDomain("example.com/67") isOfType Failure::class
+    }
+
+    @Test
+    fun `SHOULD map url to identifier`() {
+        (testSubject.mapToDomain("https://example.com/148") as Success)
+            .data mustEqual Identifier(id = "148")
+        (testSubject.mapToDomain("https://example.com/path/15") as Success)
+            .data mustEqual Identifier(id = "15")
+        (testSubject.mapToDomain("https://example.com/path/abc/5") as Success)
+            .data mustEqual Identifier(id = "5")
+        (testSubject.mapToDomain("https://example.com/path/158/889") as Success)
+            .data mustEqual Identifier(id = "889")
+        (testSubject.mapToDomain("https://example.com/path/158/abc/9999") as Success)
+            .data mustEqual Identifier(id = "9999")
+    }
+}
