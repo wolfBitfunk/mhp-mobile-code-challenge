85cb67bad135e95d27cf44480f35ffa15b412e1d
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-10-24 19:34:17 +0200

    Add PrimaryTextButton again

---
+++ app-android/src/main/java/com/wolfmontwe/mhp/challenge/mobile/android/app/presentation/design/atom/PrimaryTextButton.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022. Wolf-Martell Montwé
+ */
+
+package com.wolfmontwe.mhp.challenge.mobile.android.app.presentation.design.atom
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PrimaryTextButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    testTagName: String = "PrimaryTextButton",
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .testTag(testTagName)
+            .then(modifier),
+        colors = colors,
+    ) {
+        Text(
+            text = text.uppercase(),
+            style = MaterialTheme.typography.labelLarge
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PrimaryTextButtonPreview() {
+    PrimaryTextButton("Press here", {})
+}
