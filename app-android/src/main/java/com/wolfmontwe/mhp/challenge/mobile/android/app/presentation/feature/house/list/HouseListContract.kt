663a0c878031e43b163bd3ff1e56c9a72118ea09
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-10-24 19:38:58 +0200

    Add HouseListContract

---
+++ app-android/src/main/java/com/wolfmontwe/mhp/challenge/mobile/android/app/presentation/feature/house/list/HouseListContract.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022. Wolf-Martell Montwé
+ */
+
+package com.wolfmontwe.mhp.challenge.mobile.android.app.presentation.feature.house.list
+
+import com.wolfmontwe.mhp.challenge.mobile.android.app.domain.entity.House
+
+interface HouseListContract {
+
+    interface ViewModel {
+        fun loadMoreItems()
+        fun retry()
+    }
+
+    data class ViewState(
+        val items: List<House> = emptyList(),
+        val isLoading: Boolean = false,
+        val error: String? = null,
+        val isPagingEnd: Boolean = false,
+    )
+}
